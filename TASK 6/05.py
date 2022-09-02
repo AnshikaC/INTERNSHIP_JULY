e931ec4336c25e1cb82c8da0078a404db65186b3
AnshikaC <93823117+AnshikaC@users.noreply.github.com>
2022-09-02 18:00:47 +0530

    Add files via upload

---
+++ TASK 6/05.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+shape = tuple(map(int, input().split()))
+
+print(np.zeros(shape, int), np.ones(shape, int), sep = '\n')
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+D
